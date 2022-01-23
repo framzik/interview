7ea7ae22e03521273204e8b8c22f622c6ca1cb20
framzik <framzik@gmail.com!>
2022-01-23 21:58:37 +0300

    Revert "hw7"
    This reverts commit d08d74be1d2b89b56e87722e4e5ab2b949f55e08.

--- hw7/src/main/java/ru/khrebtov/hw7/repository/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package ru.khrebtov.hw7.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.khrebtov.hw7.entity.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Long> {
-}
